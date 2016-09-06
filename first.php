51980006dc2042aed7812feffb4489763766653f
mattbleh <mattbleh@greeny.cs.tlu.ee>
2016-09-06 15:16:45 +0300

    first php file

---
+++ first.php
@@ -0,0 +1,14 @@
+<?php
+
+	$name = "Mattias Blehner";
+	$time = "Kell on";
+	echo "Minu nimi on $name.";
+	echo '</br>';
+	echo "$time 15:23";
+		echo '</br>';
+	echo "$name";
+	echo " ";
+	echo "$time";
+echo $firstName." ".lastName;
+//punkt liidab stringe
+?>
